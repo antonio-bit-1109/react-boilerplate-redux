556c093e999d59dba1e1d48b9305b5062337ad33
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-02-03 12:42:05 +0100

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<MyMain />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/MyMain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const myMain = () => {
+    return (
+        <div>
+            {" "}
+            <h1>Hello Madafakka!</h1>
+        </div>
+    );
+};
+
+export default myMain;
