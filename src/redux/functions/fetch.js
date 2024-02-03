ae724dd56ae019d8506f653abe3cb56a10d5ecb6
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-02-03 16:57:39 +0100

    fix

--- src/redux/functions/fetch.js
+++ src/redux/functions/fetch.js
@@ -35,3 +35,95 @@ export const fetchData = (url, parametroOpzionale, options, action) => async (di
         console.error("Error fetching data:", error);
     }
 };
+
+export const fetchDelete = async (options, userId, expId) => {
+    const URLPOST = `https://striveschool-api.herokuapp.com/api/profile/${userId}/experiences/${expId}`;
+
+    try {
+        const response = await fetch(URLPOST, options);
+
+        if (!response.ok) {
+            throw new Error("qualcosa è andato storto");
+        }
+
+        return response;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const fetchDeleteCommentiHome = async (url) => {
+    const options = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            /*  Authorization: `Bearer ${TokenCommenti} `, */
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+            throw new Error("Qualcosa è andato storto");
+        }
+
+        console.log(response);
+        return response.json(); // O gestisci la risposta come preferisci
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const postImage = (userid, end, options) => async () => {
+    try {
+        // Set to true before fetching
+        const response = await fetch(`https://striveschool-api.herokuapp.com/api/profile/${userid}/${end}`, options);
+
+        if (!response.ok) {
+            if (response.status > 400 && response.status < 500) {
+                if (response.status === 429) {
+                    throw new Error("429 INFAME PER TE TANTE COSE BRUTTE");
+                } else {
+                    throw new Error("STAI CAPPELLANDO , RIGUARDA QUELLO CHE HAI SCRITTO");
+                }
+            }
+            if (response.status > 500 && response.status < 600) {
+                throw new Error("SERVER SPOMPATO, NON FUNZIA??");
+            }
+        }
+
+        const fetchData = await response.json();
+        console.log(fetchData);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+};
+
+export const fetchPutCommenti = async (datas, commentId) => {
+    const URLPUT = `https://striveschool-api.herokuapp.com/api/comments/${commentId}`;
+
+    const optionsPut = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            /*   Authorization: `Bearer ${Token}`, */
+        },
+        body: JSON.stringify(datas),
+    };
+
+    console.log("datas", datas);
+
+    try {
+        const response = await fetch(URLPUT, optionsPut);
+
+        if (!response.ok) {
+            throw new Error("qualcosa è andato storto");
+        }
+
+        console.log(response);
+        return response.json();
+    } catch (error) {
+        console.error(error);
+    }
+};
